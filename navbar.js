378ae333b013b6e05e1bd843cad1a25b0c0ab0db
Yash-Gupta <yash0314@gmail.com>
2017-08-29 18:06:30 -0700

    Mobile Version + Slideshow

---
+++ navbar.js
@@ -0,0 +1,11 @@
+
+$( "#threebar" ).click(function() {
+	$('#mobileNav').addClass("animated slideInLeft");
+	$('#mobileNav').show();
+});
+
+$( "#cross" ).click(function() {
+		$('#mobileNav').hide();
+	//});
+
+});
